ff974f8a1c21d3e08383b6056c2ce159bd414b1e
pauleatonc <pauleatonc@gmail.com>
2019-09-29 02:09:51 -0300

    from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_29_013859) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "registers", force: :cascade do |t|
     t.string "name"
     t.string "email"
